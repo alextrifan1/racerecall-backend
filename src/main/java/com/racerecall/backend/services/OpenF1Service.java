204f7d74fa45b1a47f269ffd59747e76c1f4b2b1
alextrifan1 <alexandrutrifan17@gmail.com>
2026-07-18 13:20:28 +0300

    caught case for live session

--- src/main/java/com/racerecall/backend/services/OpenF1Service.java
+++ src/main/java/com/racerecall/backend/services/OpenF1Service.java
@@ -3,6 +3,8 @@ package com.racerecall.backend.services;
 import com.racerecall.backend.models.SessionDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -20,12 +22,20 @@ public class OpenF1Service {
                 .uri("/sessions?year={year}", year)
                 .retrieve()
                 .bodyToFlux(SessionDto.class)
+                .onErrorResume(WebClientResponseException.Unauthorized.class, e -> {
+                    System.err.println("OpenF1 API locked (Live Session Ongoing) - Returning empty list.");
+                    return Flux.empty();
+                 })
                 .collectList()
                 .block();
     }
 
     public List<SessionDto> getPaginatedSession(int year, int page, int size) {
         List<SessionDto> allSessions = getSessionsByYear(year);
+
+        if (allSessions.isEmpty()) {
+            return allSessions;
+        }
 
         int start = Math.min(page * size, allSessions.size());
         int end = Math.min((page + 1) * size, allSessions.size());
